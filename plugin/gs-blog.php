3ba92c7a57ee07651ca3a130aeeb8ac0d7690133
John Stray <mail@johnstray.com>
2014-09-05 16:22:59 +0930

    Updated plugin version number

--- plugin/gs-blog.php
+++ plugin/gs-blog.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: GetSimple Blog
  * Description: A simple and easy to use blog/newsfeed for GetSimple
- * Version: 3.2.5
+ * Version: 3.3.0
  * Author: John Stray
  * Author URI: https://www.johnstray.com/
  */
@@ -11,7 +11,7 @@
 define('BLOGFILE', basename(__FILE__, ".php"));
 define('BLOGPLUGINNAME', i18n_r(BLOGFILE.'/PLUGIN_TITLE'));
 define('BLOGEXTENDID','810');
-define('BLOGVERSION','3.2.5');
+define('BLOGVERSION','3.3.0');
 require_once(BLOGFILE.'/inc/common.php');
 
 # add in this plugin's language file
