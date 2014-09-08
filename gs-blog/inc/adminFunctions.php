7210c0f9c30a3fb270654a50aa799fb51275b8e9
John Stray <mail@johnstray.com>
2014-09-08 13:07:25 +0930

    Cleaned up functions/comments
    Fixes #17

--- gs-blog/inc/adminFunctions.php
+++ gs-blog/inc/adminFunctions.php
@@ -1,14 +1,24 @@
 <?php if(!defined('IN_GS')){die('You cannot load this file directly!');} // Security Check
-/** 
-* Admin controller
-* 
-* @return void
-*/ 
-function blog_admin_controller()
-{
+/**
+ * @file: frontEndFunctions.php
+ * @package: GetSimple Blog [plugin]
+ * @action: This file contains all the functions required for displaying the blog.
+ * @author: John Stray [https://www.johnstray.id.au/]
+ */
+ 
+ 
+/**-------------------------------------------------------------------------------------------------
+ * blog_admin_controller()
+ * This functions controls what to do and where to go throughout the admin areas.
+ * 
+ * @return void (void)
+ */ 
+function blog_admin_controller() {
+
 	$Blog = new Blog;
 	getBlogUserPermissions();
-	global $blogUserPermissions;
+	GLOBAL $blogUserPermissions, $SITEURL;
+  
 	if(!isset($_GET['update'])) {
 		$update = blog_version_check();
 		if($update[0] == 'current') {$ucolor = '#308000';}
@@ -17,141 +27,78 @@ function blog_admin_controller()
 		else{$ucolor = '#D94136';}
 	} else {$ucolor = '#777777';}
 
-	if(isset($_GET['edit_post']) && $blogUserPermissions['blogeditpost'] == true)
-	{
+	if(isset($_GET['edit_post']) && $blogUserPermissions['blogeditpost'] == true) {
 		editPost($_GET['edit_post']);
-	}
-	elseif(isset($_GET['create_post']) && $blogUserPermissions['blogcreatepost'] == true)
-	{
+	} elseif(isset($_GET['create_post']) && $blogUserPermissions['blogcreatepost'] == true) {
 		editPost();
-	}
-	elseif(isset($_GET['categories']) && $blogUserPermissions['blogcategories'] == true)
-	{
-		if(isset($_GET['edit_category']))
-		{
+	} elseif(isset($_GET['categories']) && $blogUserPermissions['blogcategories'] == true) {
+		if(isset($_GET['edit_category'])) {
 			$add_category = $Blog->saveCategory($_POST['new_category']);
-			if($add_category == true)
-			{
-				echo '<div class="updated">';
-				i18n(BLOGFILE.'/CATEGORY_ADDED');
-				echo '</div>';
-			}
-			else
-			{
-				echo '<div class="error">';
-				i18n(BLOGFILE.'/CATEGORY_ERROR');
-				echo '</div>';
+			if($add_category == true) {
+				echo '<div class="updated">'.i18n_r(BLOGFILE.'/CATEGORY_ADDED').'</div>';
+			} else {
+				echo '<div class="error">'.i18n_r(BLOGFILE.'/CATEGORY_ERROR').'</div>';
 			}
 		}
-		if(isset($_GET['delete_category']))
-		{
+		if(isset($_GET['delete_category'])) {
 			$Blog->deleteCategory($_GET['delete_category']);
 		}
-		edit_categories();
-	}
-	elseif(isset($_GET['auto_importer']) && $blogUserPermissions['blogrssimporter'] == true)
-	{
-		if(isset($_POST['post-rss']))
-		{
+    #edit_categories
+		$category_file = getXML(BLOGCATEGORYFILE);
+    require_once('html/category-management.php');
+	} elseif(isset($_GET['auto_importer']) && $blogUserPermissions['blogrssimporter'] == true) {
+		if(isset($_POST['post-rss'])) {
 			$post_data = array();
 			$post_data['name'] = $_POST['post-rss'];
 			$post_data['category'] = $_POST['post-category'];
 			$add_feed = $Blog->saveRSS($post_data);
-			if($add_feed == true)
-			{
-				echo '<div class="updated">';
-				i18n(BLOGFILE.'/FEED_ADDED');
-				echo '</div>';
+			if($add_feed == true) {
+				echo '<div class="updated">'.i18n_r(BLOGFILE.'/FEED_ADDED').'</div>';
+			} else {
+				echo '<div class="error">'.i18n_r(BLOGFILE.'/FEED_ERROR').'</div>';
 			}
-			else
-			{
-				echo '<div class="error">';
-				i18n(BLOGFILE.'/FEED_ERROR');
-				echo '</div>';
-			}
-		}
-		elseif(isset($_GET['delete_rss']))
-		{
+		} elseif(isset($_GET['delete_rss'])) {
 			$delete_feed = $Blog->deleteRSS($_GET['delete_rss']);
-			if($delete_feed == true)
-			{
-				echo '<div class="updated">';
-				i18n(BLOGFILE.'/FEED_DELETED');
-				echo '</div>';
-			}
-			else
-			{
-				echo '<div class="error">';
-				i18n(BLOGFILE.'/FEED_DELETE_ERROR');
-				echo '</div>';
+			if($delete_feed == true) {
+				echo '<div class="updated">'.i18n_r(BLOGFILE.'/FEED_DELETED').'</div>';
+			} else {
+				echo '<div class="error">'.i18n_r(BLOGFILE.'/FEED_DELETE_ERROR').'</div>';
 			}
 		}
-		edit_rss();
-	}
-	elseif(isset($_GET['settings']) && $blogUserPermissions['blogsettings'] == true)
-	{
+    #edit_rss
+		$rss_file = getXML(BLOGRSSFILE);
+    require_once('html/feed-management.php');
+	} elseif(isset($_GET['settings']) && $blogUserPermissions['blogsettings'] == true) {
 		show_settings_admin();
-	}
-	elseif(isset($_GET['update']) && $blogUserPermissions['blogsettings'] == true)
-	{
+	} elseif(isset($_GET['update']) && $blogUserPermissions['blogsettings'] == true) {
 		show_update_admin();
-	}
-	elseif(isset($_GET['help']) && $blogUserPermissions['bloghelp'] == true)
-	{
-		show_help_admin();
-	}
-	elseif(isset($_GET['custom_fields']) && $blogUserPermissions['blogcustomfields'] == true)
-	{
+	} elseif(isset($_GET['help']) && $blogUserPermissions['bloghelp'] == true) {
+		require_once('html/help-admin.php');
+	} elseif(isset($_GET['custom_fields']) && $blogUserPermissions['blogcustomfields'] == true) {
 		$CustomFields = new customFields;
-		if(isset($_POST['save_custom_fields']))
-		{
+		if(isset($_POST['save_custom_fields'])) {
 			$saveCustomFields = $CustomFields->saveCustomFields();
-			if($saveCustomFields)
-			{
+			if($saveCustomFields) {
 				echo '<div class="updated">'.i18n_r(BLOGFILE.'/EDIT_OK').'</div>';
 			}
 		}
 		show_custom_fields();
-	}
-	else
-	{
-		if(isset($_GET['save_post']))
-		{
+	} else {
+		if(isset($_GET['save_post'])) {
 			savePost();
-		}
-		elseif(isset($_GET['delete_post']) && $blogUserPermissions['blogdeletepost'] == true)
-		{
+		} elseif(isset($_GET['delete_post']) && $blogUserPermissions['blogdeletepost'] == true) {
 			$post_id = urldecode($_GET['delete_post']);
 			$delete_post = $Blog->deletePost($post_id);
-			if($delete_post == true)
-			{
-				echo '<div class="updated">';
-				i18n(BLOGFILE.'/POST_DELETED');
-				echo '</div>';
-			}
-			else
-			{
-				echo '<div class="error">';
-				i18n(BLOGFILE.'/FEED_DELETE_ERROR');
-				echo '</div>';
+			if($delete_post == true) {
+				echo '<div class="updated">'.i18n_r(BLOGFILE.'/POST_DELETED').'</div>';
+			} else {
+				echo '<div class="error">'.i18n(BLOGFILE.'/FEED_DELETE_ERROR').'</div>';
 			}
 		}
-		show_posts_admin();
+    #show_posts_admin
+		$all_posts = $Blog->listPosts(true, true); // Get a list of all the posts in the blog
+    require_once('html/posts-admin.php'); // Bring in the HTML to show this section
 	}
-}
-
-/** 
-* Shows blog posts in admin panel
-* 
-* @return void
-*/  
-function show_posts_admin()
-{
-	$Blog = new Blog;
-	$all_posts = $Blog->listPosts(true, true);
-  
-  require_once('html/posts-admin.php');
-
 }
 
 /**-------------------------------------------------------------------------------------------------
@@ -201,139 +148,99 @@ function show_settings_admin() {
   }
 }
 
-/** 
-* Edit/Create post screen
-* 
-* @param $post_id string the id of the post to edit. Null if creating new page
-* @return void
-*/  
+/**-------------------------------------------------------------------------------------------------
+ * editPost($post_id)
+ * Edit/Create post screen
+ * 
+ * @param $post_id (string) The id of the post to edit. Null if creating new post
+ * @return void (void)
+ */  
 function editPost($post_id=null) {
 
 	GLOBAL $SITEURL;
 	$Blog = new Blog;
+  
 	if($post_id != null) {
 		$blog_data = getXML(BLOGPOSTSFOLDER.$post_id.'.xml');
 	} else {
 		$blog_data = $Blog->getXMLnodes();
 	}
 	require_once('html/post-editor.php');
-	include BLOGPLUGINFOLDER."ckeditor.php";
+	include(BLOGPLUGINFOLDER.'ckeditor.php');
 }
 
-/** 
-* Show Category management area
-* 
-* @return void
-*/  
-function edit_categories() {
-	$Blog = new Blog;
-	$category_file = getXML(BLOGCATEGORYFILE);
-  require_once('html/category-management.php');
-}
+/**-------------------------------------------------------------------------------------------------
+ * category_dropdown($current_category)
+ * Echos all categories to place into a select menu
+ * 
+ * @param  $current_category (string) Name of the current category. Marks it as selected.
+ * @return void              (void)
+ */  
+function category_dropdown($current_category=null) {
 
-/** 
-* RSS Feed management area
-* 
-* @return void
-*/  
-function edit_rss() {
-	$rss_file = getXML(BLOGRSSFILE);
-  require_once('html/feed-management.php');
-}
-
-/** 
-* Echos all categories to place into select menu
-* 
-* @return void
-*/  
-function category_dropdown($current_category=null)
-{
 	$category_file = getXML(BLOGCATEGORYFILE);	
-	foreach($category_file->category as $category_item)	
-	{		
+  
+	foreach($category_file->category as $category_item)	{		
 		$category_item = (string) $category_item;
-		if($category_item == $current_category)
-		{
+		if($category_item == $current_category) {
 			echo '<option value="'.$current_category.'" selected>'.$current_category.'</option>';	
-		}
-		else
-		{
+		} else {
 			echo '<option value="'.$category_item.'">'.$category_item.'</option>';	
 		}	
 	}	
-	if($current_category == null)
-	{
+  
+	if($current_category == null) {
 		echo '<option value="" selected></option>';	
-	}
-	else
-	{
+	} else {
 		echo '<option value=""></option>';	
 	}
 }
 
-/** 
-* Saves A Post
-* 
-* @return void success or error message
-*/  
-function savePost()
-{
+/**-------------------------------------------------------------------------------------------------
+ * savePost() 
+ * Saves A Post
+ * 
+ * @return void (void) success or error message
+ */  
+function savePost() {
+
 	$Blog = new Blog;
 	$xmlNodes = $Blog->getXMLnodes(true);
-	if(isset($_POST['post-title']))
-	{
-		foreach($xmlNodes as $key => $value)
-		{
-			if(!isset($_POST["post-".$key]))
-			{
-				$post_value = '';
-			}
-			else
-			{
-				$post_value = $_POST["post-".$key];
-			}
+  
+	if(isset($_POST['post-title'])) {
+		foreach($xmlNodes as $key => $value) {
+			if(!isset($_POST["post-".$key])) {$post_value = '';}
+      else {$post_value = $_POST["post-".$key];}
 			$post_data[$key] = $post_value;
 		}
 		$savePost = $Blog->savePost($post_data);
 		$generateRSS = $Blog->generateRSSFeed();
-		if($savePost != false)
-		{
-			echo '<div class="updated">';
-			i18n(BLOGFILE.'/POST_ADDED');
-			echo '</div>';
+		if($savePost != false) {
+			echo '<div class="updated">'.i18n_r(BLOGFILE.'/POST_ADDED').'</div>';
+		} else {
+			echo '<div class="error">'.i18n_r(BLOGFILE.'/POST_ERROR').'</div>';
 		}
-		else
-		{
-			echo '<div class="error">';
-			i18n(BLOGFILE.'/POST_ERROR');
-			echo '</div>';
-		}
-	}
-	else
-	{
-		echo '<div class="error">';
-		i18n(BLOGFILE.'/BLOG_CREATE_EDIT_NO_TITLE');
+	} else {
+		echo '<div class="error">'.i18n_r(BLOGFILE.'/BLOG_CREATE_EDIT_NO_TITLE');
 		echo ' <a href="javascript:history.go(-1)">'.i18n_r(BLOGFILE.'/BLOG_RETURN_TO_PREVIOUS_PAGE').'</a>';
 		echo '</div>';
 	}
 }
 
-/** 
-* Display Plugin Help
-* 
-* @return void
-*/  
-function show_help_admin() {
-	global $SITEURL; 
-	$Blog = new Blog;
-	require_once('html/help-admin.php');
-}
-
+/**-------------------------------------------------------------------------------------------------
+ * blog_theme_layouts()
+ * Returns a list of layout templates for the blog
+ * 
+ * @return $list (array) List of available templates
+ */
 function blog_theme_layouts() {
-  $files = array_filter(glob(BLOGPLUGINFOLDER.'templates/*.php'), 'is_file');
-  $list = array();
-  foreach ($files as $file) {
-    array_push($list, pathinfo($file, PATHINFO_FILENAME));
+
+  $files = array_filter(glob(BLOGPLUGINFOLDER.'templates/*.php'), 'is_file'); // Get the list of template files
+  $list = array(); // Prepare the $list array
+  
+  foreach ($files as $file) { // For each file in the list
+    array_push($list, pathinfo($file, PATHINFO_FILENAME)); // Get its name without path or extention
   }
-  return $list;
+  
+  return $list; // Return the list
 }
