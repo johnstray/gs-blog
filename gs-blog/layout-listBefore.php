0dc29569f07222300dd86cbaee7d62a990bd3f4b
John Stray <mail@johnstray.com>
2015-10-21 14:31:22 +0930

    Create layout-listBefore.php
    This allows for non-repetitive static content before the list of posts, such as a scoped stylesheet, an introduction paragraph or some advertising.

---
+++ gs-blog/layout-listBefore.php
@@ -0,0 +1,21 @@
+<!-- HTML5 Scoped StyleSheet
+     Add any css styles that you require to this style declaration. A scoped
+     stylesheet prevents your styles from contaminating the website's theme.
+     Scoped stylesheets are virtually backwards compatible with most browsers.
+-->
+<style scoped>
+  .blog_post_thumbnail {
+    width:200px;
+    height:auto;
+    float:left;
+    padding-right:10px;
+    padding-bottom:10px;
+  }
+  .blog_list_container {
+    clear:both;
+  }	
+</style>
+
+<!-- You can use the following space to write an introduction paragraph to
+     your blog, or place any static content at the top of the Blog Post List Page.
+-->
