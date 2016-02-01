3de4ce157eb2a76d85fe93d34f34166e231fcaed
John Stray <mail@johnstray.com>
2016-02-01 13:36:14 +0930

    Update gs-blog.php
    Updated version number

--- gs-blog.php
+++ gs-blog.php
@@ -11,7 +11,7 @@
 define('BLOGFILE', basename(__FILE__, ".php"));
 define('BLOGPLUGINNAME', i18n_r(BLOGFILE.'/PLUGIN_TITLE'));
 define('BLOGEXTENDID','810');
-define('BLOGVERSION','3.3.2');
+define('BLOGVERSION','3.4.0');
 require_once(BLOGFILE.'/inc/common.php');
 
 # Setup languages and language settings.
